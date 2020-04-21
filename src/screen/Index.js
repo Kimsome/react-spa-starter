da339ef438c7142d3a168fc9258b6c48d73a8c79
jinxiaolin <137185065@qq.com>
2020-04-21 17:26:21 +0800

    update:store dynamic change

--- src/screen/Index.js
+++ src/screen/Index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { inject } from 'mobx-react';
 
 import Hello from '../component/Hello';
 
+@inject("store")
 export default class Index extends Component {
     render() {
         return (
             <div>
                 <h1><Hello /></h1>
+                <div><button onClick={() => this.props.store.appname = 'Kim'}>change</button></div>
             </div>
         );
     }
